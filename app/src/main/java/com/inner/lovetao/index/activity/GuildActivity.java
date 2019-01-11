f4fb17fc090643b7f52ed809f70ce70a601f1cfd
xcz <xcz1203@163.com>
2019-01-11 18:05:20 +0800

    Fix bug + 引导页搭建

--- app/src/main/java/com/inner/lovetao/index/activity/GuildActivity.java
+++ app/src/main/java/com/inner/lovetao/index/activity/GuildActivity.java
@@ -1,12 +1,21 @@
 package com.inner.lovetao.index.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.inner.lovetao.R;
+import com.inner.lovetao.home.activity.MainActivity;
 import com.jess.arms.base.BaseActivity;
 import com.jess.arms.di.component.AppComponent;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+import cn.bingoogolapple.bgabanner.BGABanner;
 
 /**
  * desc:引导页面
@@ -14,6 +23,11 @@ import com.jess.arms.di.component.AppComponent;
  * on 2019/1/10.
  */
 public class GuildActivity extends BaseActivity {
+    @BindView(R.id.bag_banner)
+    BGABanner guildeBanner;
+    @BindView(R.id.v_togo)
+    View vTogo;
+
     @Override
     public void setupActivityComponent(@NonNull AppComponent appComponent) {
 
@@ -26,6 +40,37 @@ public class GuildActivity extends BaseActivity {
 
     @Override
     public void initData(@Nullable Bundle savedInstanceState) {
+        guildeBanner.setData(null,
+                ImageView.ScaleType.FIT_CENTER,
+                R.drawable.ic_guide_one,
+                R.drawable.ic_guide_two,
+                R.drawable.ic_guide_three);
+        guildeBanner.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
 
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                if (i == 2) {
+                    vTogo.setVisibility(View.VISIBLE);
+                } else {
+                    vTogo.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
     }
+
+    @OnClick({R.id.v_togo})
+    public void onClick() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
 }
