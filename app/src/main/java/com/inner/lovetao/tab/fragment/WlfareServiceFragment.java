6c810e57f2449a519cb34f0f3437ce379fdee5ff
xcz <xcz1203@163.com>
2019-01-28 19:37:13 +0800

    Add 福利社页面

--- app/src/main/java/com/inner/lovetao/tab/fragment/WlfareServiceFragment.java
+++ app/src/main/java/com/inner/lovetao/tab/fragment/WlfareServiceFragment.java
@@ -11,12 +11,15 @@ import com.inner.lovetao.R;
 import com.jess.arms.base.BaseFragment;
 import com.jess.arms.di.component.AppComponent;
 
+import butterknife.OnClick;
+
 /**
  * desc:福利社fragment
  * Created by xcz
  * on 2019/1/15.
  */
 public class WlfareServiceFragment extends BaseFragment {
+
     @Override
     public void setupFragmentComponent(@NonNull AppComponent appComponent) {
 
@@ -35,6 +38,37 @@ public class WlfareServiceFragment extends BaseFragment {
 
     @Override
     public void setData(@Nullable Object data) {
+
+    }
+
+    @OnClick({R.id.tv_save_master, R.id.tv_evray_day_hb, R.id.tv_coin_travel, R.id.tv_make_money_center, R.id.tv_alipay_hb, R.id.tv_open_visa, R.id.tv_want_browring, R.id.tv_want_financing})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            //收徒邀请
+            case R.id.tv_save_master:
+                break;
+            //每日红包
+            case R.id.tv_evray_day_hb:
+                break;
+            //金币兑换
+            case R.id.tv_coin_travel:
+                break;
+            //赚钱中心
+            case R.id.tv_make_money_center:
+                break;
+            //支付宝红包
+            case R.id.tv_alipay_hb:
+                break;
+            //办理信用卡
+            case R.id.tv_open_visa:
+                break;
+            //我要借钱
+            case R.id.tv_want_browring:
+                break;
+            //我要理财
+            case R.id.tv_want_financing:
+                break;
+        }
 
     }
 }
