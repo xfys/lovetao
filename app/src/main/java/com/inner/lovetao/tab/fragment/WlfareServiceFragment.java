fc341878e33c3a4630b00c1c6b743a9b4d82c2a8
xcz <xcz1203@163.com>
2019-03-09 23:10:30 +0800

    完善 福利页逻辑

--- app/src/main/java/com/inner/lovetao/tab/fragment/WlfareServiceFragment.java
+++ app/src/main/java/com/inner/lovetao/tab/fragment/WlfareServiceFragment.java
@@ -172,19 +172,6 @@ public class WlfareServiceFragment extends BaseFragment<WlfareServiceFragmentPre
 
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // TODO: inflate a fragment view
-        View rootView = super.onCreateView(inflater, container, savedInstanceState);
-        unbinder = ButterKnife.bind(this, rootView);
-        return rootView;
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        unbinder.unbind();
-    }
 
     @Override
     public void getBannerDataSu(int type, List<BannerBean> bannerBeanList) {
