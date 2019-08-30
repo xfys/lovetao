c5804340ae61b3882a0749128fb5e61434a536cd
xcz <xcz1203@163.com>
2019-08-30 16:54:55 +0800

    update gradle version

---
+++ app/src/main/java/com/inner/lovetao/MyTest.java
@@ -0,0 +1,12 @@
+package com.inner.lovetao;
+
+/**
+ * Describe:
+ *
+ * @author xcz on 2019-08-30.
+ */
+public class MyTest {
+    private void testName() {
+        System.out.println("ceshi"+"test"+"text");
+    }
+}
